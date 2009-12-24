c89dedd017bab335d22abb99ca3aabf72097c0f0
Michael M Slusarz <slusarz@curecanti.org>
2009-12-24 02:51:50 -0700

    Various git-related updates

--- source/snapshots.php
+++ source/snapshots.php
@@ -2,6 +2,5 @@
 require '../config/defaults.php';
 $toolbar_mode = 'source_snapshots';
 $content_file = './snapshots.html';
-$subsite_title = 'Horde CVS Snapshots';
+$subsite_title = 'Horde Snapshots';
 require $fs_base . '/templates/horde.inc';
-?>
