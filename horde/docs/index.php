ee00551424f8bda1e23d339d03fe298014e58568
Jan Schneider <jan@horde.org>
2010-01-19 15:00:52 +0100

    It's probably save to assume that all documentation files are UTF-8 now.

--- horde/docs/index.php
+++ horde/docs/index.php
@@ -3,7 +3,5 @@ require '../../config/defaults.php';
 $toolbar_mode  = 'horde';
 $subsite_title = 'Horde Documentation';
 $content_file  = empty($_GET['f']) ? './docs.html' : './' . basename($_GET['f']);
-if ($content_file == './po_README.html' || $content_file == './CREDITS.html') {
-    $page_charset = 'utf-8';
-}
+$page_charset = 'utf-8';
 require $fs_base . '/templates/horde.inc';
