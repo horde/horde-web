656609b668f610ad247cab82847bbaea237f341e
Chuck Hagenbuch <chuck@horde.org>
2010-11-10 10:19:53 -0500

    remove expired sponsorships

--- bounties/sponsors.php
+++ bounties/sponsors.php
@@ -18,84 +18,12 @@ $sponsors = array(
         'months'  => 12,
     ),
     array(
-        'sponsor' => 'Treefrog SEO',
-        'link'    => 'www.treefrogseo.com',
-        'contact' => 'dkidney@wi.rr.com',
-        'start'   => '2009-12-16',
-        'end'     => '2010-03-17',
-        'months'  => 3, /* Requests notice when the sponsorship will expire */
-    ),
-    array(
-        'sponsor' => 'Versicherungsvergleich Finanzvergleich',
-        'link'    => 'www.finanzvergleich.de',
-        'contact' => 'info@finanzvergleich.de',
-        'start'   => '2009-10-23',
-        'end'     => '2010-10-22',
-        'months'  => 12,
-    ),
-/*
-    array(
-        'sponsor' => 'Online Kredit Index',
-        'link'    => 'www.online-kredit-index.de',
-        'contact' => 'paul@online-kredit-index.de',
-        'start'   => '2009-11-13',
-        'end'     => '2010-11-12',
-        'months'  => 12,
-    ),
-*/
-    array(
-        'sponsor' => 'Tom\'s Gutscheine',
-        'link'    => 'www.tomsgutscheine.de',
-        'contact' => 'info@invendio.de',
-        'start'   => '2009-12-11',
-        'end'     => '2010-06-12',
-        'months'  => 6,
-    ),
-/*
-    array(
-        'sponsor' => 'Erb Hotel München',
-        'link'    => 'www.messehotel-erb-muenchen.de',
-        'contact' => 'cerb@hotel-erb.de',
-        'start'   => '2009-12-13',
-        'end'     => '2010-12-14',
-        'months'  => 12,
-    ),
-*/
-/*
-    array(
-        'sponsor' => 'Lapworld',
-        'link'    => 'www.lapworld.de',
-        'contact' => 'info@lapworld.de',
-        'start'   => '2010-02-03',
-        'end'     => '2010-08-04',
-        'months'  => 6,
-    ),
-*/
-    array(
         'sponsor' => 'Ask For Insurance',
         'link'    => 'www.askforinsurance.com',
         'contact' => 'webalex@abv.bg',
         'start'   => '2010-02-23',
         'end'     => '2011-02-24',
         'months'  => 12,
-    ),
-/*
-    array(
-        'sponsor' => 'Handylist Preisvergleich',
-        'link'    => 'handylist.de',
-        'contact' => 'nico.puhlmann@gmx.de',
-        'start'   => '2010-03-12',
-        'end'     => '2010-09-13',
-        'months'  => 6,
-    ),
-*/
-    array(
-        'sponsor' => 'LaptopPartsShop.com',
-        'link'    => 'www.laptoppartsshop.com',
-        'contact' => 'pilots@rambler.ru',
-        'start'   => '2010-07-16',
-        'end'     => '2010-09-23',
-        'months'  => 2,
     ),
 );
 
