33cbef289f9de6b04c76621e8a7b8361a5facf4b
Michael J. Rubinsky <mrubinsk@horde.org>
2011-02-14 12:43:26 -0500

    break out footer into a shared/partial

--- app/views/Home/index.html.php
+++ app/views/Home/index.html.php
@@ -241,46 +241,7 @@
 					</div>
 					<div class="clear"></div>
 				</div>
-				<div class="footer">
-					<div class="holder">
-						<div class="section">
-							<div class="add-nav">
-								<h3>Site Navigation</h3>
-								<ul>
-									<li><a href="/" class="item-home">Home</a></li>
-									<li><a href="/" class="item-download">Download</a></li>
-									<li><a href="/" class="item-wiki">Wiki</a></li>
-									<li><a href="/" class="item-support">Support</a></li>
-									<li><a href="/" class="item-contactus">Contact Us</a></li>
-								</ul>
-							</div>
-							<div class="follow-us">
-								<h3>Follow us</h3>
-								<ul>
-									<li><a target="_blank" href="http://www.twitter.com/horde" class="twitter">Twitter</a></li>
-									<li><a target="_blank" href="http://www.facebook.com/pages/horde" class="facebook">Facebook</a></li>
-								</ul>
-							</div>
-							<div class="contact-info">
-								<h3>Quick Contact</h3>
-								<dl>
-									<dt>Email:</dt>
-									<dd><a href="mailto:info@horde.org">info@horde.org</a></dd>
-									<dt>Office:</dt>
-									<dd>lorem ipsum<br>lorem ipsum<br>lorem ipsum, 1234565<br>lorem ipsum</dd>
-									<dt>Phone:</dt>
-									<dd>+123 1231 123123</dd>
-									<dt>Fax:</dt>
-									<dd>+123 1231 123123</dd>
-								</dl>
-							</div>
-							<div class="testimonials">
-								<h3>Testimonials</h3>
-								<p class="what">"Let me introduce you to one of the best webmails, Horde webmail. Horde allows you to manage your emails easily with its rich features and stability. It rocks in its performance and features."</p>
-								<p class="who">Gabrielle Capstick, Host Department</p>
-							</div>
-						</div>				
-					</div>
-					<div class="clear"></div>
-				</div>
+                <!-- Start Footer -->
+                <?php echo $this->renderPartial('footer');?>
+                <!-- End Footer -->
 			</div>
