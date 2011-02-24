57447b69f219ee843bda8147d47828baa852577a
Michael J. Rubinsky <mrubinsk@horde.org>
2011-02-24 14:18:58 -0500

    add correct url for the home link

--- app/views/shared/_banner.html.php
+++ app/views/shared/_banner.html.php
@@ -1,7 +1,7 @@
 			<div class="top">
 				<a href="/" title="Horde" class="logo"><img src="http://<?php echo $GLOBALS['host_base']?>/images/logo.png" alt="Horde"/></a>
 				<ul>
-					<li><a href="" title="Home" class="home first">Home</a></li>
+					<li><a href="<? echo $this->urlWriter->urlFor('home')?>" title="Home" class="home first">Home</a></li>
 					<li><a href="" title="Download" class="download">Download</a></li>
 					<li><a href="" title="Wiki" class="wiki">Wiki</a></li>
 					<li><a href="" title="Support" class="support">Support</a></li>
