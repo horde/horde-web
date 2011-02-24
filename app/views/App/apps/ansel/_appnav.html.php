f58b03fee5c6d376753ddd68150710a5d2f09de0
Michael J. Rubinsky <mrubinsk@horde.org>
2011-02-24 14:03:45 -0500

    At least style the temporary navbar somewhat

--- app/views/App/apps/ansel/_appnav.html.php
+++ app/views/App/apps/ansel/_appnav.html.php
@@ -1,9 +1,9 @@
 <ul class="appnav">
-    <li>Authors</li>
-    <li>Documentation</li>
-    <li>Download</li>
-    <li>Screenshots</li>
-    <li>Roadmap</li>
-    <li>Source Code</li>
+    <li><a href="#">Authors</a></li>
+    <li><a href="#">Documentation</a></li>
+    <li><a href="#">Download</a></li>
+    <li><a href="#">Screenshots</a></li>
+    <li><a href="#">Roadmap</a></li>
+    <li><a href="#">Source Code</a></li>
 </ul>
     
