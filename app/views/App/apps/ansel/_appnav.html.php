402fd3b33f284afc1b806d77a88551c4b529d7fe
Michael J. Rubinsky <mrubinsk@horde.org>
2011-02-24 16:44:30 -0500

    This *should* be able to be reused for all app pages

--- app/views/App/apps/ansel/_appnav.html.php
+++
@@ -1,9 +0,0 @@
-<ul class="appnav">
-    <li><a href="#">Authors</a></li>
-    <li><a href="#">Documentation</a></li>
-    <li><a href="#">Download</a></li>
-    <li><a href="#">Screenshots</a></li>
-    <li><a href="#">Roadmap</a></li>
-    <li><a href="#">Source Code</a></li>
-</ul>
-    
