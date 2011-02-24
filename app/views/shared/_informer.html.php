73a0d183e5326f09464bd855ec1e508089c83520
Michael J. Rubinsky <mrubinsk@horde.org>
2011-02-24 13:52:21 -0500

    Use the injector to get this. This wasn't a valid class anyway

--- app/views/shared/_informer.html.php
+++ app/views/shared/_informer.html.php
@@ -1,6 +1,6 @@
 <?php
 /* Informer widget. */
-    -$cache = new Horde_Cache_File();
+    $cache = $GLOBALS['injector']->getInstance('Horde_Cache');
     if (!($widget = $cache->get('informer', 600))) {
         $widget = @file_get_contents('http://web.informer.com/w/hordewidget.php');
         $cache->set('informer', $widget);
