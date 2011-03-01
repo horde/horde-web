d57cbd0fa6679d512ab6de4e01e8d9e0f18f03b6
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-01 11:28:50 -0500

    Add hermes
    Screenshots are *old* and must be updated before we go live.

---
+++ app/views/App/apps/hermes/hermes.html.php
@@ -0,0 +1,9 @@
+<p>Hermes is a time-tracking application integrated with the Horde
+Framework. It ties into
+<a href="<?php echo $this->urlWriter->urlFor('app', array('app' => 'turba')) ?>">
+Turba</a> (to retrieve clients) and
+<a href="<?php echo $this->urlWriter->urlFor('app', array('app' => 'nag')) ?>">
+Nag</a> and
+<a href="<?php echo $this->urlWriter->urlFor('app', array('app' => 'whups')) ?>">
+Whups</a> (to retrieve cost objects). It comes with a stop watch, search and
+reporting capabilities, and an invoice interface.</p>
