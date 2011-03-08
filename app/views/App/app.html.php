da609c4ebefaccda6491700d0363ffbd38303558
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-08 10:26:53 -0500

    remove bug sections. missed this with last commit.

--- app/views/App/app.html.php
+++ app/views/App/app.html.php
@@ -9,30 +9,13 @@
 	</div>
      <div class="rightcol" style="background: none;">
        <div id="accordion">
-         <h3><a href="#">Latest News</a></h3>
+         <h2>Latest News</h3>
          <div></div>
-         <h3><a href="#">Stable Releases</a></h3>
+         <h2>Stable Releases</h3>
          <div></div>
-         <h3><a href="#">Open Bugs</a></h3>
-         <div>
-           <?php foreach ($this->open_feed_bug as $item):?>
-             <?php echo $this->renderPartial('feedListItem', array('locals' => array('entry' => $item))); ?>
-           <?php endforeach;?>
-         </div>
-         <h3><a href="#">Open Enhancements</a></h3>
-         <div>
-           <?php foreach ($this->open_feed_enhancement as $item):?>
-             <?php echo $this->renderPartial('feedListItem', array('locals' => array('entry' => $item))); ?>
-           <?php endforeach;?></div>
        </div>
          <?php echo $this->render('sponsors');?>
     </div>
     <div class="clear"></div>
   </div>
 </div>
-<script type="text/javascript">
-  $(document).ready(function() {
-    $("#accordion").accordion({ autoHeight: false });
-  });
-
-</script>
