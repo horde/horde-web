5f8701914dd27e665df3c18dbe07f8b01feca3a4
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-22 13:41:12 -0400

    add wireframe for support landing page

---
+++ app/views/Support/index.html.php
@@ -0,0 +1,16 @@
+<div class="content">
+  <div class="main">
+    <div class="mainareasplit">
+        <h2><span></span>Support</h2>
+        <div class="section">
+         <div class="sectionintro">
+         </div>
+        </div>
+
+    </div>
+    <div class="rightcol" style="background: none;">
+      <?php echo $this->render('sponsors');?>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
