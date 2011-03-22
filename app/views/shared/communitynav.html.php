03100f4a284d9ce00017c9da03103aa94eca782d
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-22 14:19:46 -0400

    fix link to applications in interior nav

--- app/views/shared/communitynav.html.php
+++ app/views/shared/communitynav.html.php
@@ -1,5 +1,5 @@
 <ul class="communitynav">
     <li><a href="<?php echo $this->urlWriter->urlFor('community')?>">Community Home</a></li>
-    <li><?php echo $this->linkToUnlessCurrent('Applications', array('controller' => 'community', 'action' => 'apps'))?></li>
+    <li><?php echo $this->linkToUnlessCurrent('Applications', array('controller' => 'app'))?></li>
     <li><?php echo $this->linkToUnlessCurrent('Community Support', array('controller' => 'community', 'action' => 'support'))?></li>
 </ul>
