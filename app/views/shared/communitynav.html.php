b8baab951ac2ffeb010cc30786bda1ca20874d53
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-22 14:44:39 -0400

    Add localition link to community nav, add missing breadcrumbs

--- app/views/shared/communitynav.html.php
+++ app/views/shared/communitynav.html.php
@@ -1,5 +1,7 @@
 <ul class="communitynav">
     <li><a href="<?php echo $this->urlWriter->urlFor('community')?>">Community Home</a></li>
     <li><?php echo $this->linkToUnlessCurrent('Applications', array('controller' => 'app'))?></li>
+    <li><?php echo $this->linkToUnlessCurrent('Localization', array('controller' => 'community', 'action' => 'localization'))?></li>
+
     <li><?php echo $this->linkToUnlessCurrent('Community Support', array('controller' => 'community', 'action' => 'support'))?></li>
 </ul>
