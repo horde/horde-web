304fd0eb866aa9fbc0d7784aa6789a6f1ace2f39
Michael J. Rubinsky <mrubinsk@horde.org>
2011-03-23 01:09:11 -0400

    Add links to major sections in the footer as well.
    After working with the site for a while, it's obvious these are needed.
    It's frustrating not being able to jump from one section to another from
    the internal pages.

--- app/views/shared/footer.html.php
+++ app/views/shared/footer.html.php
@@ -5,6 +5,9 @@
 								<h3>Site Navigation</h3>
 								<ul>
 									<li><a href="<? echo $this->urlWriter->urlFor('home')?>" title="Home" class="home">Home</a></li>
+									<li><a href="<? echo $this->urlWriter->urlFor('community')?>" title="Community" class="community">Community</a></li>
+                                    <li><a href="<? echo $this->urlWriter->urlFor('development')?>" title="Development" class="home">Development</a></li>
+                                    <li><a href="<? echo $this->urlWriter->urlFor('services')?>" title="Services" class="services">Services</a></li>
 									<li><a href="<?php echo $this->urlWriter->urlFor('download')?>" title="Download" class="download">Download</a></li>
 									<li><a href="http://wiki.horde.org" title="Wiki" class="wiki">Wiki</a></li>
                                     <li><a href="<?php echo $this->urlWriter->urlFor('support')?>" title="Support" class="support">Support</a></li>
