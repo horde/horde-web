47ed3c7d3259dad6dab8f8bdb79b7dc3a1d2332a
Ben Klang <bklang@mojolingo.com>
2011-03-26 16:49:15 -0400

    Update my info

--- app/views/shared/authors/Ben_Klang.html.php
+++ app/views/shared/authors/Ben_Klang.html.php
@@ -2,8 +2,8 @@
   <h2>Ben Klang</h2>
   <div style="float:right;"><img src="http://www.gravatar.com/avatar/0bc29a0e5e6fc84e02620dbb2d562ae5" /></div>
   <ul>
-    <li>I work for <a href="http://www.systemefficiency.com/">System Efficiency</a>, the PeopleSoft Enterprise Experts.</li>
-    <li>My Horde contributions are primarily on the WebDAV functionality, LDAP integration, the Horde_Cache SQL driver and service management applications (Beatnik, Vilma, Operator).  I also administer the Horde Developer CVS server.</li>
+    <li>I am a Horde Core developer and one of the founding members of Horde LLC.</li>
+    <li>My Horde contributions are primarily on the LDAP integration, WebDAV/CalDAV functionality, the Horde_Cache SQL driver and service management applications (Beatnik, Vilma, Operator).  I also administer the Horde Developer servers.</li>
     <li>My Other Open Source Projects: <a href="http://projects.alkaloid.net/">Alkaloid Networks Projects</a></li>
     <li>When I need to let off some steam I beat my drum in The Band: <a href="http://www.seedandfeed.org">Seed &amp; Feed Marching Abominable</a></li>
   </ul>
