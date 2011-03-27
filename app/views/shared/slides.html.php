0ee44c2c05331d5a7eb5157a805f1f35d9b0216b
Ben Klang <bklang@mojolingo.com>
2011-03-26 20:21:08 -0400

    Update slide content

--- app/views/shared/slides.html.php
+++ app/views/shared/slides.html.php
@@ -1,23 +1,28 @@
             <div class="slider" id="slides">
                 <div class="slides_container">
-                    <div class="slide1 sliderview">
-                        <h2>Create. Communicate. Collaborate.</h2>
-                        <p>The Horde Project is about creating high quality Open
-                           Source applications, based on PHP and the Horde Framework.
-                           The guiding principles of the Horde Project are to create solid standards-based applications using intelligent object oriented design that, wherever possible, are designed to run on a wide range of platforms and backends.
-                           There is great emphasis on making Horde as friendly to non-English speakers as possible. The Horde Framework currently supports many localization features such as unicode and right-to-left text and generous users have contributed many translations for the framework and applications.</p>
-                    </div>
-                    <div class="slide1 sliderview">
+                    <div class="slide-CCC sliderview">
                         <h2>Create. Communicate. Collaborate.</h2>
                         <p>Horde Groupware Webmail Edition is a free, enterprise
-                           ready, browser based communication suite. Users can
-                           read, send and organize email messages and manage and
-                           share calendars, contacts, tasks and notes with the
-                           standards compliant components from the Horde Project.
-                           Horde Groupware Webmail Edition bundles the
+                           ready, browser-based communication suite. Users can
+                           send, read and organize email messages and manage and
+                           share calendars, contacts, tasks and notes with users both inside
+                           and beyond their own network. Horde Groupware Webmail Edition bundles the
                            separately available applications IMP, Ingo,
                            Kronolith, Turba, Nag and Mnemo.</p>
                         
                     </div>
+
+                    <div class="slide-Everywhere sliderview">
+                        <h2>Horde Everywhere.</h2>
+                        <p>Horde gives you the ability to access your information no matter where you are &ndash; On the road using your mobile phone; at your desk with our powerful browser-based interface; in the datacenter linking all your business systems together.  The flexibility to get the right information to the right people at the right time.</p>
+                    </div>
+
+                    <div class="slide-Groupware sliderview">
+                        <h2>Open Source, Standards-Based.</h2>
+                        <p>The Horde Project is about creating high quality Open
+                           Source applications, based on PHP and the Horde Framework.
+                           The guiding principle of the Horde Project is to deliver robust, standards-based applications capable of using existing platforms and backends.  Re-use your existing investments in user bases and databases.  Our intelligent, object oriented design will perform on a wide range of infrastructure.</p>
+                    </div>
+
                 </div>
             </div>
