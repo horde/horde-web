8f0b31ff7d791538399372655a350be6f5ade116
Michael J. Rubinsky <mrubinsk@horde.org>
2011-04-02 12:53:28 -0400

    freshen up my profile as well

--- app/views/shared/authors/Michael_Rubinsky.html.php
+++ app/views/shared/authors/Michael_Rubinsky.html.php
@@ -2,11 +2,18 @@
   <h2>Michael J Rubinsky</h2>
   <div style="float:right;"><img src="http://www.gravatar.com/avatar/fb930e44282b804151c29212eebaa883" /></div>
   <ul class="hordeauthor">
-      <li>Graduate of <a href="http://www.usip.edu" />Philadelphia College of Pharmacy and Science</a> (since renamed to University of the Sciences in Philadelphia).</li>
-      <li>Joined the Horde team in 2004 when my IMSP classes were incorporated into the Horde Framework.</li>
-      <li>I contribute to a number of Horde applications and libraries but am focused primarily on Ansel at the moment.</li>
-      <li>In my spare time I enjoy <a href="http://rubinskyfamily.com/photos/gallery.php?gallery=3279">flying small aircraft</a>, <a href="http://rubinskygallery.com">black and white photography</a> and spending time with my <a href="http://rubinskyfamily.com">family</a></li>
-      <li>Website: <a href="http://theupstairsroom.com">http://theupstairsroom.com</a>.</li>
-      <li>Personal: <a href="http://rubinskyfamily.com/mike">http://rubinskyfamily.com</a>.</li>
+      <li>Graduate of <a href="http://www.usip.edu" />Philadelphia College of
+          Pharmacy and Science</a> (since renamed to University of the Sciences
+          in Philadelphia).</li>
+      <li>Core developer and a founding member of Horde, LLC.</li>
+      <li>In my spare time I enjoy
+          <a href="http://rubinskyfamily.com/photos/gallery.php?gallery=3279">
+          flying small aircraft</a>, <a href="http://rubinskygallery.com">black
+          and white photography</a> and spending time with my
+          <a href="http://rubinskyfamily.com">family</a></li>
+      <li>Website: <a href="http://theupstairsroom.com">
+          http://theupstairsroom.com</a>.</li>
+      <li>Personal: <a href="http://rubinskyfamily.com/mike">
+          http://rubinskyfamily.com</a>.</li>
   </ul>
 </div>
