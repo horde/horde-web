a2d0fddccaf716a58dfc3cf9289129991d21de93
Michael J. Rubinsky <mrubinsk@horde.org>
2011-04-03 17:01:07 -0400

    update Horde app page

--- app/views/App/apps/horde/horde.html.php
+++ app/views/App/apps/horde/horde.html.php
@@ -7,9 +7,7 @@ browser detection, connection tracking, MIME handling, and more.</p>
 
 <p>The Horde Framework can be obtained from our
 <a href="<?php echo $this->urlWriter->urlFor('download', array('app' => 'horde')) ?>">
-Download Page</a>. There are also some
-<a href="<?php echo $this->urlWriter->urlFor('themes') ?>">themes</a> that users
-have created.</p>
+Download Page</a>.</p>
 
 <p>You can also see who is on the <a href="<?php echo $this->urlWriter->urlFor('team') ?>">
 Core Team</a>.</p>
@@ -22,10 +20,10 @@ Core Team</a>.</p>
 
 <h3>Notes</h3>
 
-<p>Horde makes heavy use of <a href="http://pear.php.net/">PEAR</a>, the
-<b>P</b>HP <b>E</b>xtension and <b>A</b>pplication <b>R</b>epository. PEAR is
-a set of reuseable PHP components providing things such as Logging, Database
-abstraction, and much more. Horde's
-<a href="<?php echo Horde::Url($this->urlWriter->urlFor('app', array('app' => 'horde', 'action' => 'docs')))->add('f', 'INSTALL.html') ?>">INSTALL
-File</a> provides instructions on how to correctly configure your system for
-use with PEAR.</p>
+<p>As of Horde 4.0, The Horde Framework libraries and all applications,
+including the base Horde application (which provides core
+functionality), are installable via <a href="http://pear.horde.org">PEAR</a>.
+See the <a href="http://pear.horde.org">PEAR</a> server page, or the Horde
+application
+<?php echo Horde::Url($this->urlWriter->urlFor(array('controller' => 'app', 'app' => 'horde', 'action' => 'docs')))->add('f', 'INSTALL.html')->link()?>
+INSTALL</a> file for more information.</p>
