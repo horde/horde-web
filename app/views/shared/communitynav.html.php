c91e4a24218c0d94efc1b7d9ad03ee090eb02dab
Jan Schneider <jan@horde.org>
2011-04-03 18:36:56 +0200

    Use linkTo() instead of linkToUnlessCurrent(), at least until we have a visual distinction.

--- app/views/shared/communitynav.html.php
+++ app/views/shared/communitynav.html.php
@@ -1,6 +1,6 @@
 <ul class="communitynav">
-    <li><a href="<?php echo $this->urlWriter->urlFor('community')?>">Community Home</a></li>
-    <li><?php echo $this->linkToUnlessCurrent('Applications', array('controller' => 'app'))?></li>
-    <li><?php echo $this->linkToUnlessCurrent('Localization', array('controller' => 'community', 'action' => 'localization'))?></li>
-    <li><?php echo $this->linkToUnlessCurrent('Community Support', array('controller' => 'community', 'action' => 'support'))?></li>
+  <li><a href="<?php echo $this->urlWriter->urlFor('community')?>">Community Home</a></li>
+  <li><?php echo $this->linkTo('Applications', array('controller' => 'app'))?></li>
+  <li><?php echo $this->linkTo('Localization', array('controller' => 'community', 'action' => 'localization'))?></li>
+  <li><?php echo $this->linkTo('Community Support', array('controller' => 'community', 'action' => 'support'))?></li>
 </ul>
--- app/views/shared/developmentnav.html.php
+++ app/views/shared/developmentnav.html.php
@@ -1,9 +1,9 @@
 <ul class="communitynav">
-  <li><?php echo $this->linkToUnlessCurrent('Contribute', array('controller' => 'development', 'action' => 'contribute')) ?></li>
+  <li><?php echo $this->linkTo('Contribute', array('controller' => 'development', 'action' => 'contribute')) ?></li>
   <li><a href="http://git.horde.org">Browse Source</a></li>
-  <li><?php echo $this->linkToUnlessCurrent('Docs', array('controller' => 'development', 'action' => 'docs'))?></li>
-  <li><?php echo $this->linkToUnlessCurrent('CVS', array('controller' => 'development', 'action' => 'cvs'))?></li>
-  <li><?php echo $this->linkToUnlessCurrent('Git', array('controller' => 'development', 'action' => 'git'))?></li>
-  <li><?php echo $this->linkToUnlessCurrent('Modules', array('controller' => 'development', 'action' => 'modules'))?></li>
-  <li><?php echo $this->linkToUnlessCurrent('Versions', array('controller' => 'development', 'action' => 'versions'))?></li>
+  <li><?php echo $this->linkTo('Docs', array('controller' => 'development', 'action' => 'docs'))?></li>
+  <li><?php echo $this->linkTo('CVS', array('controller' => 'development', 'action' => 'cvs'))?></li>
+  <li><?php echo $this->linkTo('Git', array('controller' => 'development', 'action' => 'git'))?></li>
+  <li><?php echo $this->linkTo('Modules', array('controller' => 'development', 'action' => 'modules'))?></li>
+  <li><?php echo $this->linkTo('Versions', array('controller' => 'development', 'action' => 'versions'))?></li>
 </ul>
