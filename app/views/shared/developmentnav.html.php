da84ad3a0505eeb93bb96e6f8c71ff17cd38acf5
Jan Schneider <jan@horde.org>
2011-04-03 18:37:29 +0200

    Move Git link first.

--- app/views/shared/developmentnav.html.php
+++ app/views/shared/developmentnav.html.php
@@ -2,8 +2,8 @@
   <li><?php echo $this->linkTo('Contribute', array('controller' => 'development', 'action' => 'contribute')) ?></li>
   <li><a href="http://git.horde.org">Browse Source</a></li>
   <li><?php echo $this->linkTo('Docs', array('controller' => 'development', 'action' => 'docs'))?></li>
-  <li><?php echo $this->linkTo('CVS', array('controller' => 'development', 'action' => 'cvs'))?></li>
   <li><?php echo $this->linkTo('Git', array('controller' => 'development', 'action' => 'git'))?></li>
+  <li><?php echo $this->linkTo('CVS', array('controller' => 'development', 'action' => 'cvs'))?></li>
   <li><?php echo $this->linkTo('Modules', array('controller' => 'development', 'action' => 'modules'))?></li>
   <li><?php echo $this->linkTo('Versions', array('controller' => 'development', 'action' => 'versions'))?></li>
 </ul>
