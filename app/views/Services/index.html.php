b6dd538e8b55c83ae7f95816e2f2375843e253bb
Ben Klang <bklang@horde.org>
2011-04-05 19:38:23 -0400

    Marketing message

--- app/views/Services/index.html.php
+++ app/views/Services/index.html.php
@@ -3,6 +3,29 @@
     <div class="mainareasplit">
         <h2><span></span>Services</h2>
         <?php echo $this->render('servicesnav');?>
+        <div class="section">
+        <h3 class="exciter">More than just software</h3>
+        <p class="exciter">Our team of trusted professionals helps you build large installations or custom applications.</p>
+
+        <h3>Why Horde Groupware?</h3>
+        <p>Horde is the most flexible groupware on the planet, and that makes it the perfect platform on which to build your business productivity software:</p>
+        <ul>
+            <li>Horde connects to (and officially supports) more backends (SQL, LDAP, IMAP, Sieve, SMTP, caching systems, etc) than any other groupware system.  This means you can continue to use your existing userbase and your users do not have to remember any additional passwords.</li>
+            <li>Data entered in Horde may be updated or consumed via a wide range of front-end protocols (HTTP/HTML, DAV, SyncML, ActiveSync, iCalendar, more).  Why let your data be siloed?  Horde gets presents your information where you need it.</li>
+            <li>Template and CSS-based design makes customization easy.  There are nearly unlimited numbers of ways to customize and configure Horde to fit almost any need.</li>
+            <li>Maturity. Horde's source code goes back over 10 years and benefits from years of engineering, bugfixes, security reports testing and user feedback.</li>
+            <li>Feature-rich. Horde's feature set rivals the largest enterprise email systems.  We have an incredible feature set that compares favorably against any competition in our primary apps (mail, calendar address book), and even more in our secondary apps (time tracking, wiki, ticketing, and more).</li>
+            <li>Open Source. Horde is open source, which means there are never per-user licensing fees or costly mandatory support contracts.  It also means that, if necessary, you can customize any part of Horde at any time.</li>
+
+            <li>Active development. Horde benefits from a large and active community of developers and users, and continues to add features nearly every day.</li>
+        </ul>
+
+        <h3>Why Choose Horde LL?</h3>
+        <p>Horde LLC was founded by the people that have spent the past 10 years creating the Horde Groupware Suite.  We are experts in our craft, and have a track record of helping some of the largest Horde deployments in the world (100,000 users to 5,000,000 users in some cases!) deploy Horde successfully.  Who better to ask than the experts?</p>
+
+        <h3>Contact us for a free assessment of your project</h3>
+        <p>We love to talk shop.  Give us a call at <span style="font-weight: bold;">+1.404.475.4830</span> or drop us a line at <a href="mailto:info@horde.org" style="font-weight: bold;">info@horde.org</a>.  We will set up a time to discuss your project and help you establish a roadmap for achieving your objectives.</p>
+        </div>
     </div>
     <div class="rightcol" style="background: none;"><?php echo $this->render('sponsors'); ?></div>
     <div class="clear"></div>
