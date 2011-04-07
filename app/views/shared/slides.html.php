883fae495ed63d9ff08969bde29f9f7a1a5ca49b
Jan Schneider <jan@horde.org>
2011-04-07 22:06:59 +0200

    Tweak class names, put mobile first.

--- app/views/shared/slides.html.php
+++ app/views/shared/slides.html.php
@@ -1,6 +1,11 @@
             <div class="slider" id="slides">
                 <div class="slides_container">
-                    <div class="slide-CCC sliderview">
+                    <div class="slide-everywhere sliderview">
+                        <h2>Horde Everywhere.</h2>
+                        <p>Horde gives you the ability to access your information no matter where you are &ndash; On the road using your mobile phone; at your desk with our powerful browser-based interface; in the datacenter linking all your business systems together.  The flexibility to get the right information to the right people at the right time.</p>
+                    </div>
+
+                    <div class="slide-ccc sliderview">
                         <h2>Create. Communicate. Collaborate.</h2>
                         <p>Horde Groupware Webmail Edition is a free, enterprise
                            ready, browser-based communication suite. Users can
@@ -12,12 +17,7 @@
                         
                     </div>
 
-                    <div class="slide-Everywhere sliderview">
-                        <h2>Horde Everywhere.</h2>
-                        <p>Horde gives you the ability to access your information no matter where you are &ndash; On the road using your mobile phone; at your desk with our powerful browser-based interface; in the datacenter linking all your business systems together.  The flexibility to get the right information to the right people at the right time.</p>
-                    </div>
-
-                    <div class="slide-Groupware sliderview">
+                    <div class="slide-oss sliderview">
                         <h2>Open Source, Standards-Based.</h2>
                         <p>The Horde Project is about creating high quality Open
                            Source applications, based on PHP and the Horde Framework.
