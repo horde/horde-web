d12a564558d5fdc4fcab93f8d6e7de18b8e45154
Michael J. Rubinsky <mrubinsk@horde.org>
2011-04-07 11:56:37 -0400

    add screenshot page for kronolith.
    Still need dynamic view screenshots for K3

---
+++ app/views/App/apps/kronolith/appscreenshots.html.php
@@ -0,0 +1,87 @@
+<!--<h4>Kronolith 3.0, Dynamic View</h4>
+
+<table border="0">
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/ajax-month', true) ?><br />
+        Month View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/ajax-week', true) ?><br />
+        Week View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/ajax-tasks', true) ?><br />
+    Tasks View
+    </td>
+</tr>
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/ajax-event', true) ?><br />
+    Event View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/ajax-calendar', true) ?><br />
+    Calendar View
+    </td>
+    <td align="center">
+    </td>
+</tr>
+</table>-->
+
+
+<h3>Kronolith 3.0, Mobile View</h3>
+<table border="0">
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/mobile-month', true) ?><br />
+        Month View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/mobile-day', true) ?><br />
+        Day View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', '3/mobile-event', true) ?><br />
+    Event View
+    </td>
+</tr>
+</table>
+
+<h3>Kronolith 2.0, with Horde's Mozilla theme:</h3>
+<table border="0">
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'month', true) ?><br />
+    Month View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'workweek', true) ?><br />
+    Week View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'day', true) ?><br />
+    Day View
+    </td>
+</tr>
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'view', true) ?><br />
+    Event View
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'edit', true) ?><br />
+    Editing an event
+    </td>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'invite', true) ?><br />
+    Inviting people to an event
+    </td>
+</tr>
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('kronolith', 'mouseover', true) ?><br />
+    Event descriptions mouseover
+    </td>
+</tr>
+</table>
