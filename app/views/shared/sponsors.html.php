205680ce9cf0b9baa55a2d38e575372e49d7f528
Ben Klang <bklang@horde.org>
2011-04-08 12:41:51 -0400

    Fix path to logos

--- app/views/shared/sponsors.html.php
+++ app/views/shared/sponsors.html.php
@@ -3,9 +3,9 @@
                             <?php echo $this->render('informer'); ?><br />
                             <ul class="ads">
                                 <li><a href=""><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" /></a></li>
-                                <li><a href=""><img src="http://www.horde.org/graphics/logos/easyDNS.gif" /></a></li>
-                                <li><a href=""><img src="http://www.horde.org/graphics/logos/hub.org.gif" height="31"/></a></li>
-                                <li><a href=""><img src="http://www.horde.org/graphics/logos/verendus.jpg" height="31"/></a></li>
+                                <li><a href=""><img src="http://www.horde.org/images/easyDNS.gif" /></a></li>
+                                <li><a href=""><img src="http://www.horde.org/images/hub.gif" height="31"/></a></li>
+                                <li><a href=""><img src="http://www.horde.org/images/verendus.jpg" height="31"/></a></li>
                             </ul>
                             <div class="clear"></div>
                             <div class="thanks"><?php echo $this->linkTo('Thanks', array('controller' => 'home', 'action' => 'thanks')); ?></div>
