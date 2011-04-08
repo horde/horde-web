89f7c7a39b6248d8dbb2fdcbdf1c3c857fcb0d88
Jan Schneider <jan@horde.org>
2011-04-09 01:11:31 +0200

    Smush images.

--- app/views/shared/sponsors.html.php
+++ app/views/shared/sponsors.html.php
@@ -3,8 +3,8 @@
                             <?php echo $this->render('informer'); ?><br />
                             <ul class="ads">
                                 <li><a href=""><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" /></a></li>
-                                <li><a href=""><img src="<?php echo $GLOBALS['host_base'] ?>/images/easyDNS.gif" /></a></li>
-                                <li><a href=""><img src="<?php echo $GLOBALS['host_base'] ?>/images/hub.gif" height="31"/></a></li>
+                                <li><a href=""><img src="<?php echo $GLOBALS['host_base'] ?>/images/easyDNS.png" /></a></li>
+                                <li><a href=""><img src="<?php echo $GLOBALS['host_base'] ?>/images/hub.png" height="31"/></a></li>
                                 <li><a href=""><img src="<?php echo $GLOBALS['host_base'] ?>/images/verendus.jpg" height="31"/></a></li>
                             </ul>
                             <div class="clear"></div>
