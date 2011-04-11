af8dc968fa1c0e85ce6f5ba7d01eacd02d3edb15
Michael J. Rubinsky <mrubinsk@horde.org>
2011-04-11 15:28:07 -0400

    link this to the apps page

--- app/views/shared/banner.html.php
+++ app/views/shared/banner.html.php
@@ -2,7 +2,7 @@
     <a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Horde" class="logo"><img src="<?php echo $GLOBALS['host_base']?>/images/logo.png" alt="Horde"/></a>
     <ul>
         <li><a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Home" class="home first">Home</a></li>
-        <li><a href="<?php echo $this->urlWriter->urlFor('download')?>" title="Download" class="download">Download</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('apps')?>" title="Download" class="download">Download</a></li>
         <li><a href="http://wiki.horde.org" title="Wiki" class="wiki">Wiki</a></li>
         <li><a href="<?php echo $this->urlWriter->urlFor('support')?>" title="Support" class="support">Support</a></li>
         <li><a href="<?php echo $this->urlWriter->urlFor('contact')?>" title="Contact Us" class="contactus last">Contact Us</a></li>
