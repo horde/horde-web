39bd74cf3fd0548600111e20035c636a6472858e
Jan Schneider <jan@horde.org>
2011-05-23 22:44:06 +0200

    Move fb button to More section.

--- app/views/shared/sponsors.html.php
+++ app/views/shared/sponsors.html.php
@@ -20,10 +20,9 @@
     <li><a href="http://www.hub.org/?ri=765"><img src="<?php echo $GLOBALS['host_base'] ?>/images/hub.png" height="31"/></a></li>
     <li><a href="http://www.verendus.com"><img src="<?php echo $GLOBALS['host_base'] ?>/images/verendus.jpg" height="31"/></a></li>
   </ul>
-  <iframe src="http://www.facebook.com/plugins/like.php?app_id=132513070157462&amp;href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FHorde-LLC%2F192707224109487&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
-  <br />
-  <br />
   <h2>More</h2>
+  <iframe src="http://www.facebook.com/plugins/like.php?app_id=132513070157462&amp;href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FHorde-LLC%2F192707224109487&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+  <br /><br />
   <?php echo $this->linkTo('Logos', array('controller' => 'home', 'action' => 'logos'));?><br />
   <?php echo $this->linkTo('Thanks', array('controller' => 'home', 'action' => 'thanks')); ?>
 </div>
