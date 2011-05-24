3592cea767635e84e6da6f742088a08bbb9141cc
Jan Schneider <jan@horde.org>
2011-05-24 14:36:06 +0200

    Fix indention and closing tag.

--- app/views/App/app.html.php
+++ app/views/App/app.html.php
@@ -1,28 +1,28 @@
 <div class="content">
   <div class="main">
     <div class="mainareasplit">
-        <?php echo $this->renderPartial('appbreadcrumb') ?>
-        <span class="appMenu"><?php echo $this->render('appnav') ?></span>
-        <div class="section">
-          <?php echo $this->render($this->appname) ?>
-        </div>
+      <?php echo $this->renderPartial('appbreadcrumb') ?>
+      <span class="appMenu"><?php echo $this->render('appnav') ?></span>
+      <div class="section">
+        <?php echo $this->render($this->appname) ?>
+      </div>
     </div>
-     <div class="rightcol" style="background: none;">
-       <div id="accordion">
-         <h2>Latest News</h3>
-         <ul class="sidebar">
-            <?php $i = 0; ?>
-            <?php foreach ($this->latestNews as $entry):
-               if ($i++ >= 5) {
-                   break;
-               }
-              echo $this->renderPartial('feedListItem', array('locals' => array('entry' => $entry)));
-              endforeach;
-            ?>
-         </ul>
-         <div></div>
-       </div>
-         <?php echo $this->render('sponsors');?>
+    <div class="rightcol" style="background: none;">
+      <div id="accordion">
+        <h2>Latest News</h2>
+        <ul class="sidebar">
+          <?php $i = 0; ?>
+          <?php foreach ($this->latestNews as $entry):
+          if ($i++ >= 5) {
+              break;
+          }
+          echo $this->renderPartial('feedListItem', array('locals' => array('entry' => $entry)));
+          endforeach;
+          ?>
+        </ul>
+        <div></div>
+      </div>
+      <?php echo $this->render('sponsors');?>
     </div>
     <div class="clear"></div>
   </div>
