d33a1df75efc7c94df771cfb3fdacde460bb1158
Jan Schneider <jan@horde.org>
2011-07-01 16:59:59 +0200

    Add German phone number.

--- app/views/shared/contact.html.php
+++ app/views/shared/contact.html.php
@@ -6,6 +6,9 @@
         <dt>Postal Address:</dt>
         <dd>P.O. Box 79322<br>Atlanta, GA<br>30307<br>United States</dd>
         <dt>Phone:</dt>
-        <dd>+1.404.475.4830</dd>
+        <dd>
+          +1.404.475.4830 (US)<br>
+          +49.521.4469.8995 (DE)
+        </dd>
     </dl>
 </div>
