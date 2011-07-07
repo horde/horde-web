fe07a7f1c77cc0f37e4c5edab5ecf08c277bfa4a
Jan Schneider <jan@horde.org>
2011-07-07 11:19:47 +0200

    Add TOC.

--- app/views/Community/support.html.php
+++ app/views/Community/support.html.php
@@ -82,6 +82,7 @@
       </div>
     </div>
     <div class="rightcol" style="background: none;">
+      <?php echo $this->render('toc'); ?>
       <?php echo $this->render('sponsors');?>
     </div>
     <div class="clear"></div>
