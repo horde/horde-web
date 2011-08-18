b11077425aa951fa292686136cb5bd988cca7414
Jan Schneider <jan@horde.org>
2011-08-18 11:19:45 +0200

    Fix FAQ links.

--- app/views/shared/authors/Eric_Rostetter.html.php
+++ app/views/shared/authors/Eric_Rostetter.html.php
@@ -5,7 +5,7 @@
         <a href="http://www.utexas.edu/">The University of Texas at Austin</a>
         <a href="http://www.ph.utexas.edu/">Department of Physics</a></li>
     <li>Graduate of <a href="http://www.utoledo.edu/">The University of Toledo</a></li>
-    <li>Horde <a href="<?php echo $base_url ?>/faq/">FAQ</a> maintainer</li>
+    <li>Horde <a href="http://wiki.horde.org/FAQ">FAQ</a> maintainer</li>
     <li>(Very outdated) web page: <a href="http://www.ph.utexas.edu/~ericr/">http://www.ph.utexas.edu/~ericr/</a></li>
   </ul>
 </div>
