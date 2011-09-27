eb59761e4fddb6355601964bb0917f344c47c914
Jan Schneider <jan@horde.org>
2011-09-27 14:55:39 +0200

    Add/extend feature list.

--- app/views/App/apps/wicked/wicked.html.php
+++ app/views/App/apps/wicked/wicked.html.php
@@ -1,11 +1,19 @@
-<p>
-Wicked is a Wiki for the Horde framework. It uses PEAR's <a
-href="http://pear.php.net/Text_Wiki">Text_Wiki</a> package for markup
-rules, parsing, and rendering.  If you're interested in helping, feel
-free to join the <a href="<?php echo $base_url ?>/mail/">Wicked
-mailing list</a>.
-</p>
-<p>
-Wicked is also powering the official <a href="http://wiki.horde.org">Horde
-Wiki</a>.
-</p>
+<p>Wicked is a Wiki for the Horde framework. It uses
+PEAR's <a href="http://pear.php.net/Text_Wiki">Text_Wiki</a> package for markup
+rules, parsing, and rendering. It supports Creole, Mediawiki, Tiki Wiki, and
+its own markup engine.</p>
+
+<p>Some of Wicked's features are:</p>
+
+<ul>
+  <li>Configurable markup engine</li>
+  <li>HTML, plain text, Latex, reStructuredText output rendering</li>
+  <li>Change tracking and revision history</li>
+  <li>Full-text searching</li>
+  <li>Per-page permissions</li>
+  <li>Attachments with versioning</li>
+  <li>Wiki synchronization</li>
+</ul>
+
+<p>Wicked is also powering the official <a href="http://wiki.horde.org">Horde
+Wiki</a>.</p>
