a71947578bca9206c480b7db85624558b96cb8d6
Jan Schneider <jan@horde.org>
2011-10-06 19:02:59 +0200

    Restore old font style, spell out Applications, but use flexible LI width.

--- app/views/shared/banner.html.php
+++ app/views/shared/banner.html.php
@@ -1,8 +1,8 @@
 <div class="top">
     <a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Horde" class="logo"><img src="<?php echo $GLOBALS['host_base']?>/images/logo.png" alt="Horde"/></a>
     <ul>
-        <li><a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Home" class="home first">Home</a></li>
-        <li><a href="<?php echo $this->urlWriter->urlFor('apps')?>" title="Applications" class="applications">Apps</a></li>
+        <li id="home"><a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Home" class="home first">Home</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('apps')?>" title="Applications" class="applications">Applications</a></li>
         <li><a href="http://wiki.horde.org" title="Wiki" class="wiki">Wiki</a></li>
         <li><a href="http://demo.horde.org" title="Demo" class="demo">Demo</a></li>
         <li><a href="<?php echo $this->urlWriter->urlFor('support')?>" title="Support" class="support">Support</a></li>
