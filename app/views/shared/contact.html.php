c1040c23626fe8e92440de4fa6aeb4c63cbe7851
Gunnar Wrobel <p@rdus.de>
2011-11-13 20:47:54 +0100

    Remove phone contact for now.

--- app/views/shared/contact.html.php
+++ app/views/shared/contact.html.php
@@ -5,10 +5,5 @@
         <dd><a href="mailto:info@horde.org">info@horde.org</a></dd>
         <dt>Postal Address:</dt>
         <dd>P.O. Box 79322<br>Atlanta, GA<br>30307<br>United States</dd>
-        <dt>Phone:</dt>
-        <dd>
-          +1.404.475.4830 (US)<br>
-          +49.521.4469.8995 (DE)
-        </dd>
     </dl>
 </div>
