c016c142123c95121828d19f660a32e02d86e94c
Michael J Rubinsky <mrubinsk@horde.org>
2011-12-05 12:44:40 -0500

    Update Ansel's page:
    Update inline thumbnails
    Add thumbnail for iPhoto Exporter
    Add download information for Aperture/iPhoto exporters.

--- app/views/App/apps/ansel/ansel.html.php
+++ app/views/App/apps/ansel/ansel.html.php
@@ -1,26 +1,38 @@
 <p><div style="float:left;margin:3px;"><a title="Image View" href="<?php echo $this->urlWriter->urlFor('app', array('app' => 'ansel', 'action' => 'screenshots')) ?>">
-<img alt="Image View" src="<?php echo $GLOBALS['host_base']?>/images/screenshots/ansel/imageview_thumb.jpg" /></a></div>
+<img alt="Image View" src="<?php echo $GLOBALS['host_base']?>/images/screenshots/ansel/2/imageview-thumb.jpg" /></a></div>
 
 <p>Ansel is a full featured photo management application. With it, you can create
 any number of galleries and subgalleries, share galleries among other Horde
 users or even make them public. You can upload images either one at a time, as a
-zip archive, or even upload them via Windows XP's "Publish to Web"
-functionality. Once in Ansel, you can browse your images, download originals,
-view galleries as a slideshow, add comments to images, send an 'ecard' to a
-friend, and more. You can crop, resize and perform other image maniputlation
-functions.  You can even have Ansel try to recognize people in your photos
-with it's support for face detection. Ansel will read and store EXIF data
-associated with uploaded images and can even auto rotate your images to the
-proper position if the image contains the proper EXIF properties.</p>
+zip archive, upload them via Windows XP's "Publish to Web", or use one of the
+available export plugins (see below). Once in Ansel, you can browse your images, download
+originals, view galleries as a slideshow, add comments to images, send an
+'ecard' to a friend, and more. You can crop, resize and perform other image
+maniputlation functions.  You can even have Ansel try to detect faces in your
+photos. Ansel will read and store EXIF data associated with uploaded images and
+can even auto rotate your images to the proper position if the image contains
+the proper EXIF properties.</p>
 
 <div style="float:right;margin:3px;"><a href="screenshots/index.php" title="Polaroid Style Gallery View">
-<img alt="Polaroid Style Gallery View" src="<?php echo $GLOBALS['host_base']?>/images/screenshots/ansel/polaroidgallery_thumb.jpg" /></a></div>
+<img alt="Polaroid Style Gallery View" src="<?php echo $GLOBALS['host_base']?>/images/screenshots/ansel/2/polaroidstacks-thumb.jpg" /></a></div>
+
+<p>As mentioned above,
+<a href="<?php echo $this->urlWriter->urlFor(array('controller' => 'download', 'action' => 'app', 'app' =>'ansel'))?>">
+export plugins</a> are available for both iPhoto and Aperture.
+Both Ansel versions 1.x and 2.x are supported by these uploaders. All metadata
+is retained during export, including keywords. You can create new galleries
+directly from the plugin, as well as browse a gallery's thumbnails so you can
+what images have been previously uploaded. You may configure multiple Ansel
+servers as well.</p>
 
 <p>Ansel supports photo and gallery tagging - allowing you to browse your photos by
 tags just like you were browsing a directory. You can also choose to have
 certain EXIF tags automatically added to an image on upload. As of Ansel 1.1,
 geotagging is included. Geotags can be obtained from existing EXIF location data
 or can be added directly in Ansel via Google Maps integration.</p>
+
+<div style="float:left;margin:3px;"><a href="screenshots/index.php" title="iPhoto Plugin">
+<img alt="iPhoto Plugin" src="<?php echo $GLOBALS['host_base']?>/images/screenshots/ansel/plugin-thumb.png" /></a></div>
 
 <p>Mulitple gallery themes are available. You can create thumbnails that have
 rounded corners with shadows, sharp corners with shadows, or even make your
---
+++ app/views/Download/apps/ansel/additional.html.php
@@ -0,0 +1,12 @@
+<h3>Related Downloads</h3>
+
+<p>Plugins are also available for uploading pictures directly to Ansel from both
+Aperture and iPhoto. Both Ansel versions 1.x and 2.x are supported by these
+uploaders.</p>
+
+<ul>
+ <li><a href="ftp://ftp.horde.org/pub/iPhoto2Ansel/iPhoto2Ansel-1.0.0alpha1.pkg">
+ iPhoto Plugin</a> Requires OS X 10.5 or later, has been tested with iPhoto 08 and 09.</li>
+ <li><a href="ftp://ftp.horde.org/pub/ApertureToAnselExportPlugin/ApertureToAnsel-1.0.0alpha1.pkg">
+ Aperture Plugin</a> Requires OS x 10.5 or later, tested with Aperture 2.1 through 3.2.1.</li>
+</ul>
