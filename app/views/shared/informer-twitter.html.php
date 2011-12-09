64dd0ddd51bdcf770136df993f06fe11da076bf1
Jan Schneider <jan@horde.org>
2011-12-09 18:02:50 +0100

    Indent

--- app/views/shared/informer-twitter.html.php
+++ app/views/shared/informer-twitter.html.php
@@ -1,9 +1,8 @@
 <?php
 /* Informer twitter widget. */
-    $cache = $GLOBALS['injector']->getInstance('Horde_Cache');
-    if (!($widget = $cache->get('informertwitter', 600))) {
-        $widget = @file_get_contents('http://67.228.51.211/twidgethor.php');
-        $cache->set('informertwitter', $widget);
-    }
-    echo $widget;
-?>
+$cache = $GLOBALS['injector']->getInstance('Horde_Cache');
+if (!($widget = $cache->get('informertwitter', 600))) {
+    $widget = @file_get_contents('http://67.228.51.211/twidgethor.php');
+    $cache->set('informertwitter', $widget);
+}
+echo $widget;
