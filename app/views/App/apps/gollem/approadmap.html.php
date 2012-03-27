83126d66f4982184a4b3db89a7ac7cee1c7133d9
Jan Schneider <jan@horde.org>
2012-03-27 18:58:31 +0200

    Add Gollem roadmap.

---
+++ app/views/App/apps/gollem/approadmap.html.php
@@ -0,0 +1,27 @@
+<h3>Gollem Roadmaps</h3>
+<p>
+The following roadmaps summarize the important changes planned for
+future releases.  They do not list all changes, only changes that
+are considered significant from the end user's point of view.
+In addition to the changes listed, most releases will probably contain
+at least some of the following changes:
+</p>
+<ul>
+  <li>Bug fixes, performance tweaks, code cleanups, etc.</li>
+  <li>New and/or improved translations.</li>
+  <li>New preferences or configuration options.</li>
+</ul>
+
+<h3>Gollem 3.0</h3>
+
+<p><b>Status:</b> In development</p>
+
+<p><b>Planned release timeframe:</b> April, 2012</p>
+
+<p><b>Features currently planned for the next release:</b></p>
+
+<table cellspacing="0" class="roadmap">
+  <tr><th>Feature</th><th>Status</th>
+  <tr><td>File sharing</td><td class="done">Done</td></tr>
+  <tr><td>Reactivate WebDAV access</td><td class="planned">Planned</td>
+</table>
