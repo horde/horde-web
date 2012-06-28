8639bf70f32712fc6db9b086778b1d2522a05914
Ralf Lang <lang@b1-systems.de>
2012-06-28 05:32:33 +0200

    Adding a view for sesha to the website.

---
+++ app/views/App/apps/sesha/appauthors.html.php
@@ -0,0 +1,4 @@
+<?php
+echo $this->render('Jan_Schneider');
+echo $this->render('Ralf_Lang');
+echo $this->render('Bo_Daley');
---
+++ app/views/App/apps/sesha/sesha.html.php
@@ -0,0 +1,4 @@
+<p>Sesha is the Horde Inventory Manager. It is an application designed to track a
+multitude of items. It can organize stockable items into multiple categories,
+each with unique properties.
+</p>
