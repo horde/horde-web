3b5047271f8192159aad186735bd760ab7a7f135
Ralf Lang <lang@b1-systems.de>
2012-07-10 15:16:41 +0200

    Some Screenshots for sesha (more to come)

---
+++ app/views/App/apps/sesha/appscreenshots.html.php
@@ -0,0 +1,22 @@
+<h3>Sesha H5 (1.0)</h3>
+<table border="0">
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('sesha', 'sesha1') ?><br />
+        Adding a new property
+    </td>
+</tr>
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('sesha', 'sesha2-addinventory') ?><br />
+        Adding an item to the inventory
+    </td>
+</tr>
+<tr>
+    <td align="center">
+    <?php echo HordeWeb_Utils::sslink('sesha', 'sesha3-viewinventory') ?><br />
+    Gallery View
+    </td>
+</tr>
+</table>
+
