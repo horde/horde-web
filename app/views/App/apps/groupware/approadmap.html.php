0ce3c8622b8c47c08ed0955d18135bf8ff0481c2
Jan Schneider <jan@horde.org>
2013-02-12 16:27:20 +0100

    Add groupware roadmaps.

---
+++ app/views/App/apps/groupware/approadmap.html.php
@@ -0,0 +1,46 @@
+<h3>Horde Groupware Roadmaps</h3>
+<p>
+The following roadmaps summarize the important changes planned for
+future releases.  They do not list all changes, only changes that
+are considered significant from the end user's point of view.
+In addition to the changes listed, most releases will probably contain
+at least some of the following changes:
+</p>
+<ul>
+  <li>Bug fixes, performance tweaks, code cleanups, etc.</li>
+  <li>New and/or improved translations.</li>
+  <li>New preferences or configuration options.</li>
+</ul>
+
+<h3>Horde Groupware 6.0</h3>
+
+<p><b>Status:</b> Planned</p>
+
+<p><b>Planned release timeframe:</b> Open</p>
+
+<p><b>Features currently planned for the next release:</b></p>
+
+<table cellspacing="0" class="roadmap">
+  <tr><th>Feature</th><th>Status</th>
+  <tr><td>Task dependencies</td><td class="planned">Planned</td>
+</table>
+
+<h3>Horde Groupware Webmail Edition 5.1</h3>
+
+<p><b>Status:</b> In development</p>
+
+<p><b>Planned release timeframe:</b> April 2013</p>
+
+<p><b>Features currently planned for the next release:</b></p>
+
+<table cellspacing="0" class="roadmap">
+  <tr><th>Feature</th><th>Status</th>
+  <tr><td>Add file manager</td><td class="progress">In progress</td></tr>
+  <tr><td>Add bookmark manager</td><td class="progress">In progress</td></tr>
+  <tr><td>CalDAV server capabilities</td><td class="progress">In progress</td></tr>
+  <tr><td>CardDAV support</td><td class="progress">In progress</td>
+  <tr><td>Merge Vacation drivers (forwards, ldap, qmail, sql)</td><td class="planned">Planned</td>
+  <tr><td>Merge Vacation alias drivers (none, ftp)</td><td class="planned">Planned</td>
+  <tr><td>Merge Forwards drivers (forwards, ldap, mdaemon, qmail, soap, sql, customsql)</td><td class="planned">Planned</td>
+  <tr><td>Composite filter driver to manage different backends for different services</td><td class="progress">In Progress</td>
+</table>
