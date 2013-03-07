b9b3bc71032fd260fc59ab44fa9b60cfe4d28b21
Michael M Slusarz <slusarz@curecanti.org>
2013-03-06 23:18:12 -0700

    Update IMP 6 screenshots

--- app/views/App/apps/imp/appscreenshots.html.php
+++ app/views/App/apps/imp/appscreenshots.html.php
@@ -2,63 +2,40 @@
 <table>
  <tr>
   <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_1', true) ?><br />
-   IMP 6.0 Main View
+   <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_dynamic', true) ?><br />
+   Main (Mailbox) View
   </td>
   <td align="center">
    <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_2', true) ?><br />
-   IMP 6.0 Compose Window
+   Compose Window
   </td>
 </table>
 
-<h3>IMP 5 Standard (IMP) Message View [MIME Torture Test 2]</h3>
+<h3>IMP 6 Smartmobile View</h3>
 <table>
  <tr>
   <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_1', true) ?><br />
-   IMP 5.0
+   <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_smartmobile', true) ?><br />
+   Main (Mailbox) View
   </td>
-  <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_2', true) ?><br />
-   IMP 5.0
-  </td>
- </tr>
 </table>
 
-<h3>IMP 5 Standard (IMP) Mailbox View [Threaded Sort]</h3>
+<h3>IMP 6 Basic View</h3>
 <table>
  <tr>
   <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_3', true) ?><br />
-   IMP 5.0
+   <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_basic', true) ?><br />
+   Main (Mailbox) View
   </td>
- </tr>
 </table>
 
-<h3>IMP 5 Dynamic (DIMP) Mailbox/Preview View</h3>
+<h3>IMP 6 Minimal View</h3>
 <table>
  <tr>
   <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_4', true) ?><br />
-   IMP 5.0
+   <?php echo HordeWeb_Utils::sslink('imp', '6/imp6_minimal', true) ?><br />
+   Main (Mailbox) View
   </td>
- </tr>
 </table>
 
-<h3>IMP 5 Mobile Views</h3>
-<table>
- <tr>
-  <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_5', true) ?><br />
-   Folders list
-  </td>
-  <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_6', true) ?><br />
-   Mailbox view
-  </td>
-  <td align="center">
-   <?php echo HordeWeb_Utils::sslink('imp', '5/imp5_7', true) ?><br />
-   Message view
-  </td>
- </tr>
-</table>
+<a href="<?php echo $this->urlWriter->urlFor('app', array('action' => 'screenshots_old', 'app' => 'imp'))?>">Screenshots from older versions</a>
