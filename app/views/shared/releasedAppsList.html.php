55ca5d20af8065d84c31cd5c1d32dfce9831a8e1
Jan Schneider <jan@horde.org>
2014-03-11 09:55:00 +0100

    Forgot to add the readable application name.

--- app/views/shared/releasedAppsList.html.php
+++ app/views/shared/releasedAppsList.html.php
@@ -1,6 +1,6 @@
 <h2>Released Applications</h2>
 <ul class="sidebar">
-<?php foreach ($this->stable as $key => $app):?>
- <li><a href="<?php echo $this->urlWriter->urlFor(array_merge($this->appListController, array('app' => $key))) ?>"><?php echo $app['name']?></a></li>
+<?php foreach ($this->stable as $app):?>
+ <li><a href="<?php echo $this->urlWriter->urlFor(array_merge($this->appListController, array('app' => $app['application']))) ?>"><?php echo $app['name']?></a></li>
 <?php endforeach;?>
 </ul>
