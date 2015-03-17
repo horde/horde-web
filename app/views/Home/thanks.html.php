b057b4aea8dee41216f767a7620997434d141115
Jan Schneider <jan@horde.org>
2015-03-17 13:33:42 +0100

    New sponsor.

--- app/views/Home/thanks.html.php
+++ app/views/Home/thanks.html.php
@@ -35,6 +35,11 @@
             <img style="margin-bottom:5px" src="<?php echo $GLOBALS['host_base'] ?>/images/logo_sponsor_beslist.jpg" height="40" />
           </a>
         </p>
+        <p style="text-align:center">
+          <a style="margin:20px;text-decoration:none" href="http://www.chameleonjohn.com/" target="_blank">
+            <img src="<?php echo $GLOBALS['host_base'] ?>/images/logo_sponsor_chameleon-john.jpg" height="40"/>
+          </a>
+        </p>
         <br/>
 
         <h3>New sponsors</h3>
