f73420ed555a83ad1260c0dfd3c9c7393c3a1d52
Michael J Rubinsky <mrubinsk@horde.org>
2016-03-09 22:54:38 -0500

    Move to the newer analytics.js api since ga.js is deprecated.

--- app/views/layout/home.html.php
+++ app/views/layout/home.html.php
@@ -7,6 +7,14 @@
 <link type="text/css" rel="stylesheet" href="<?php echo $GLOBALS['host_base'] ?>/css/horde.css" media="screen">
 <link rel="SHORTCUT ICON" type="image/x-icon" href="<?php echo $GLOBALS['host_base'] ?>/images/favicon.ico" />
 <link href="https://plus.google.com/105569801098474752113" rel="publisher" />
+<!-- Google Analytics -->
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-22320801-1', 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='//www.google-analytics.com/analytics.js'></script>
+<!-- End Google Analytics -->
 </head>
 <body>
   <div class="area">
@@ -19,15 +27,8 @@
 <script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
 <script type="text/javascript" src="js/slides.min.jquery.js"></script>
 <script type="text/javascript" src="js/informer.js"></script>
-<?php if (strpos($_SERVER['SERVER_NAME'], 'horde.org') !== false): ?>
-<script type="text/javascript" src="<?php if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') echo 'https://ssl'; else echo 'http://www' ?>.google-analytics.com/ga.js"></script>
-<?php endif ?>
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 <script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-22320801-1']);
-_gaq.push(['_trackPageview']);
-
 $(function(){
     $('#slides').slides({
         preload: true,
--- app/views/layout/main.html.php
+++ app/views/layout/main.html.php
@@ -7,6 +7,14 @@
 <link type="text/css" rel="stylesheet" href="<?php echo $GLOBALS['host_base'] ?>/css/horde.css" media="screen">
 <link rel="SHORTCUT ICON" type="image/x-icon" href="<?php echo $GLOBALS['host_base'] ?>/images/favicon.ico" />
 <link href="https://plus.google.com/105569801098474752113" rel="publisher" />
+<!-- Google Analytics -->
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-22320801-1', 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='//www.google-analytics.com/analytics.js'></script>
+<!-- End Google Analytics -->
 <?php $GLOBALS['injector']->getInstance('Horde_PageOutput')->includeStylesheetFiles(array('nobase' => true, 'nohorde' => true), true) ?>
 </head>
 <body>
@@ -20,16 +28,8 @@
 </div>
 <?php $GLOBALS['injector']->getInstance('Horde_PageOutput')->includeScriptFiles() ?>
 <?php $GLOBALS['injector']->getInstance('Horde_PageOutput')->outputInlineScript() ?>
-<?php if (strpos($_SERVER['SERVER_NAME'], 'horde.org') !== false): ?>
-<script type="text/javascript" src="<?php if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') echo 'https://ssl'; else echo 'http://www' ?>.google-analytics.com/ga.js"></script>
-<?php endif ?>
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['host_base'] ?>/js/informer.js"></script>
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-22320801-1']);
-_gaq.push(['_trackPageview']);
-</script>
 <script type="text/javascript" src="<?php echo $GLOBALS['host_base'] ?>/js/toc.js"></script>
 </body>
 </html>
