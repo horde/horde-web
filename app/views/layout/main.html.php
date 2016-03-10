ec82759a2efba0e200fca0c2df98afe1ffb1a8c7
Michael J Rubinsky <mrubinsk@horde.org>
2016-03-09 23:16:16 -0500

    Use correct js snippet, verified via analytics.

--- app/views/layout/main.html.php
+++ app/views/layout/main.html.php
@@ -9,11 +9,13 @@
 <link href="https://plus.google.com/105569801098474752113" rel="publisher" />
 <!-- Google Analytics -->
 <script>
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-22320801-1', 'auto');
-ga('send', 'pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-22320801-1', 'auto');
+  ga('send', 'pageview');
 </script>
-<script async src='//www.google-analytics.com/analytics.js'></script>
 <!-- End Google Analytics -->
 <?php $GLOBALS['injector']->getInstance('Horde_PageOutput')->includeStylesheetFiles(array('nobase' => true, 'nohorde' => true), true) ?>
 </head>
