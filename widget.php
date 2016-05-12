3e69e3dfe3febde529a7db54dfbef5282f020b3f
Michael J Rubinsky <mrubinsk@horde.org>
2016-05-11 22:49:47 -0400

    Update widget.php - use refererUrl, not HTTP_REFERRER

--- widget.php
+++ widget.php
@@ -1,4 +1,3 @@
 <?php
 //http://web.horde.org/widget/sidebarJS?url=http://horde.org
-echo @file_get_contents('http://web.horde.org/widget/sidebarJS?url=' . @$_GET['url'] . '&refererUrl=' . @$_SERVER['HTTP_REFERRER']);
-?>
+echo @file_get_contents('http://web.horde.org/widget/sidebarJS?url=' . @$_GET['url'] . '&refererUrl=' . @$_GET['refererUrl']);
