ad7bdbe852118826f179acb5880ad235ee3d58fb
Jan Schneider <jan@horde.org>
2016-06-08 12:04:02 +0200

    Fix short open tags.

--- app/views/shared/footer.html.php
+++ app/views/shared/footer.html.php
@@ -3,10 +3,10 @@
     <div class="add-nav">
       <h3>Site Navigation</h3>
       <ul>
-        <li><a href="<? echo $this->urlWriter->urlFor('home')?>" title="Home" class="home">Home</a></li>
-        <li><a href="<? echo $this->urlWriter->urlFor('community')?>" title="Community" class="community">Community</a></li>
-        <li><a href="<? echo $this->urlWriter->urlFor('development')?>" title="Development" class="home">Development</a></li>
-        <li><a href="<? echo $this->urlWriter->urlFor('services')?>" title="Services" class="services">Services</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('home')?>" title="Home" class="home">Home</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('community')?>" title="Community" class="community">Community</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('development')?>" title="Development" class="home">Development</a></li>
+        <li><a href="<?php echo $this->urlWriter->urlFor('services')?>" title="Services" class="services">Services</a></li>
         <li><a href="<?php echo $this->urlWriter->urlFor('apps')?>" title="Applications" class="applications">Applications</a></li>
         <li><a href="http://wiki.horde.org" title="Wiki" class="wiki">Wiki</a></li>
         <li><a href="http://demo.horde.org" title="Demo" class="demo">Demo</a></li>
