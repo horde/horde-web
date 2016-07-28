90283aa5ea142f9ab00e0dbba324bad6852dd0b8
Jan Schneider <jan@horde.org>
2016-07-28 16:59:49 +0200

    Use a larger button.

--- app/views/shared/sponsors.html.php
+++ app/views/shared/sponsors.html.php
@@ -10,7 +10,7 @@
      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
      <input type="hidden" name="cmd" value="_s-xclick">
      <input type="hidden" name="hosted_button_id" value="DWV8RS3BHMVC2">
-     <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+     <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
      </form>
    </div>
