15e54a6bbb70876ed6ae37db1d81275c2ac74e7e
Michael J Rubinsky <mrubinsk@horde.org>
2017-02-21 09:46:52 -0500

    Remove no longer used template.

--- app/views/shared/informer-twitter.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Informer twitter widget. */
-$cache = $GLOBALS['injector']->getInstance('Horde_Cache');
-if (!($widget = $cache->get('informertwitter', 600))) {
-    $widget = @file_get_contents('http://whois.stsoftware.biz/twidgethor.php');
-    $cache->set('informertwitter', $widget);
-}
-echo $widget;
