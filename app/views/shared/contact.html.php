2d0647e6710c5ab344d635dc54b163093da4b823
Jan Schneider <jan@horde.org>
2017-04-04 11:58:56 +0200

    Add h-card markup too.
    The old markup was hCard. Yes, there is a difference.

--- app/views/shared/contact.html.php
+++ app/views/shared/contact.html.php
@@ -1,25 +1,25 @@
 <div class="contact-info">
   <h3>Quick Contact</h3>
-  <dl class="vcard">
+  <dl class="h-card vcard">
     <dt>Email:</dt>
-    <dd><a href="mailto:info@horde.org" class="email">info@horde.org</a></dd>
+    <dd><a href="mailto:info@horde.org" class="u-email email">info@horde.org</a></dd>
     <dt>Postal Address:</dt>
     <dd>
-      <span class="fn">Horde LLC</span><br>
-      <span class="adr">
-        <span class="street-address">
+      <span class="p-name fn">Horde LLC</span><br>
+      <span class="h-adr adr">
+        <span class="p-street-address street-address">
           691 John Wesley<br>
           Dobbs Ave, Unit C
         </span><br>
-        <span class="locality">Atlanta</span>, <span class="region">GA</span><br>
-        <span class="postal-code">30312</span><br>
-        <span class="country-name">United States</span>
+        <span class="p-locality locality">Atlanta</span>, <span class="p-region region">GA</span><br>
+        <span class="p-postal-code postal-code">30312</span><br>
+        <span class="p-country-name country-name">United States</span>
       </span>
     </dd>
     <dt>Phone:</dt>
     <dd>
-      <a href="tel:+1.404.400.1661" class="tel">+1.404.400.1661</a> (US)<br>
-      <a href="tel:+49.521.4469.8995" class="tel">+49.521.4469.8995</a> (DE)
+      <a href="tel:+1.404.400.1661" class="p-tel tel">+1.404.400.1661</a> (US)<br>
+      <a href="tel:+49.521.4469.8995" class="p-tel tel">+49.521.4469.8995</a> (DE)
     </dd>
   </dl>
 </div>
