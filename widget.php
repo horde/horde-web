2422bbb33b7ccd063180f36bdce38eede3dbc949
Michael J Rubinsky <mrubinsk@horde.org>
2018-09-01 10:55:30 -0400

    Update http -> https for horde.org links.

--- widget.php
+++ widget.php
@@ -1,4 +1,4 @@
 <?php
-//http://web.horde.org/widget/sidebarJS?url=http://horde.org
+//http://web.horde.org/widget/sidebarJS?url=https://horde.org
 header('Content-Type: application/javascript; charset=utf-8');
 echo @file_get_contents('http://web.horde.org/widget/sidebarJS?url=' . @$_GET['url'] . '&refererUrl=' . @$_GET['refererUrl']);
