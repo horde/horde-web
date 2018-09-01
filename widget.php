3d7ea8caa3399060dc18529343ec3f925e1872d9
Michael J Rubinsky <mrubinsk@horde.org>
2018-09-01 11:04:35 -0400

    More seo cleanup.

--- widget.php
+++
@@ -1,4 +0,0 @@
-<?php
-//http://web.horde.org/widget/sidebarJS?url=https://horde.org
-header('Content-Type: application/javascript; charset=utf-8');
-echo @file_get_contents('http://web.horde.org/widget/sidebarJS?url=' . @$_GET['url'] . '&refererUrl=' . @$_GET['refererUrl']);
