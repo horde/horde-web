196508b8a4b2abea30f5823d558995498dbf965f
Jan Schneider <jan@horde.org>
2019-04-09 22:35:16 +0200

    Remove expired sponsors.

--- app/views/Home/thanks.html.php
+++ app/views/Home/thanks.html.php
@@ -26,17 +26,9 @@
           <a style="margin:20px;text-decoration:none" href="http://psw.net/" target="_blank">
             <img src="<?php echo $GLOBALS['host_base'] ?>/images/sponsors/psw.gif" height="40" alt="PSW GROUP" />
           </a>
-          <a style="margin:20px;text-decoration:none" href="http://canadianwebhosting.com/" target="_blank">
-            <img src="<?php echo $GLOBALS['host_base'] ?>/images/sponsors/cwh.png" height="40" alt="Canadian Web Hosting" />
-          </a>
           <a style="margin:20px;text-decoration:none" href="https://www.topratedbingosites.co.uk/" target="_blank">
             <img src="<?php echo $GLOBALS['host_base'] ?>/images/sponsors/topratedbingosites.jpg" height="40" alt="topratedbingosites.co.uk" />
           </a>
-<?php if (date('Ymd') <= 20190203): ?>
-          <a style="margin:20px;text-decoration:none" href="https://hostpresto.com/?utm_source=horde" target="_blank">
-            <img src="<?php echo $GLOBALS['host_base'] ?>/images/sponsors/hostpresto.png" height="40" alt="HostPresto!" />
-          </a>
-<?php endif ?>
         </p>
         <br/>
 
