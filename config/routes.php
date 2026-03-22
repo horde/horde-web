8877d4837aef04b11f0231384707c76623fed177
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-22 07:15:44 +0100

    chore(routes): Update all route definitions to builder proxy.

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,6 @@
  *
  */
 
-use Horde\Routes\RouteBuilder;
 use Horde\Hordeweb\Controller\Home;
 use Horde\Hordeweb\Controller\Community;
 use Horde\Hordeweb\Controller\Licenses;
@@ -18,206 +17,158 @@ use Horde\Hordeweb\Controller\Shop;
 
 $_root = ltrim(dirname($_SERVER['PHP_SELF']), '/');
 
-// Home controller routes (modern RouteBuilder pattern)
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/'))
-        ->withName('home')
-        ->withController(Home::class)
-        ->withAction('index')
-);
+/* Home controller routes */
+$mapper->route($_root . '/')
+    ->withName('home')
+    ->withController(Home::class)
+    ->withAction('index')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/contact/'))
-        ->withName('contact')
-        ->withController(Home::class)
-        ->withAction('contact')
-);
+$mapper->route($_root . '/contact/')
+    ->withName('contact')
+    ->withController(Home::class)
+    ->withAction('contact')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/thanks'))
-        ->withName('thanks')
-        ->withController(Home::class)
-        ->withAction('thanks')
-);
+$mapper->route($_root . '/thanks')
+    ->withName('thanks')
+    ->withController(Home::class)
+    ->withAction('thanks')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/logos'))
-        ->withName('logos')
-        ->withController(Home::class)
-        ->withAction('logos')
-);
+$mapper->route($_root . '/logos')
+    ->withName('logos')
+    ->withController(Home::class)
+    ->withAction('logos')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/410'))
-        ->withController(Home::class)
-        ->withAction('410')
-);
+$mapper->route($_root . '/410')
+    ->withName('410')
+    ->withController(Home::class)
+    ->withAction('410')
+    ->add();
 
-// Mail
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/mail'))
-        ->withName('mail')
-        ->withController(Home::class)
-        ->withAction('mail')
-);
+$mapper->route($_root . '/mail')
+    ->withName('mail')
+    ->withController(Home::class)
+    ->withAction('mail')
+    ->add();
 
 /* Community */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/community'))
-        ->withName('community')
-        ->withController(Community::class)
-        ->withAction('index')
-);
+$mapper->route($_root . '/community/:action')
+    ->withName('community')
+    ->withController(Community::class)
+    ->defaults('action', 'index')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/community/:action'))
-        ->withController(Community::class)
-);
+$mapper->route($_root . '/community/team')
+    ->withName('team')
+    ->withController(Community::class)
+    ->withAction('team')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/community/localization'))
-        ->withName('localization')
-        ->withController(Community::class)
-        ->withAction('localization')
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/community/team'))
-        ->withName('team')
-        ->withController(Community::class)
-        ->withAction('team')
-);
+$mapper->route($_root . '/community/localization')
+    ->withName('localization')
+    ->withController(Community::class)
+    ->withAction('localization')
+    ->add();
 
 /* Licenses */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/licenses'))
-        ->withName('licenses')
-        ->withController(Licenses::class)
-        ->withAction('index')
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/licenses/:action'))
-        ->withController(Licenses::class)
-);
+$mapper->route($_root . '/licenses/:action')
+    ->withName('licenses')
+    ->withController(Licenses::class)
+    ->defaults('action', 'index')
+    ->add();
 
 /* Support */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/support'))
-        ->withName('support')
-        ->withController(Support::class)
-        ->withAction('index')
-);
+$mapper->route($_root . '/support')
+    ->withName('support')
+    ->withController(Support::class)
+    ->withAction('index')
+    ->add();
 
 /* Apps */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps'))
-        ->withName('apps')
-        ->withController(App::class)
-        ->withAction('index')
-);
+$mapper->route($_root . '/apps')
+    ->withName('apps')
+    ->withController(App::class)
+    ->withAction('index')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps/:app/docs/:file'))
-        ->withController(App::class)
-        ->withAction('docs')
-);
+$mapper->route($_root . '/apps/:app/docs/:file')
+    ->withName('app_docs')
+    ->withController(App::class)
+    ->withAction('docs')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps/:app'))
-        ->withName('app')
-        ->withController(App::class)
-        ->withAction('app')
-);
+$mapper->route($_root . '/apps/:app')
+    ->withName('app')
+    ->withController(App::class)
+    ->withAction('app')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps/:app/:action'))
-        ->withController(App::class)
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps/:app/screenshots'))
-        ->withController(App::class)
-        ->withAction('screenshots')
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/apps/:app/screenshots_old'))
-        ->withController(App::class)
-        ->withAction('screenshots_old')
-);
+$mapper->route($_root . '/apps/:app/:action')
+    ->withName('app_action')
+    ->withController(App::class)
+    ->defaults('action', 'app')
+    ->withSecondaryRoute($_root . '/apps/:app/screenshots')
+    ->withSecondaryRoute($_root . '/apps/:app/screenshots_old')
+    ->add();
 
 /* Libraries */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/libraries'))
-        ->withName('libraries')
-        ->withController(Library::class)
-        ->withAction('index')
-);
+$mapper->route($_root . '/libraries')
+    ->withName('libraries')
+    ->withController(Library::class)
+    ->withAction('index')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/libraries/:library/docs/:file'))
-        ->withController(Library::class)
-        ->withAction('docs')
-);
+$mapper->route($_root . '/libraries/:library/docs/:file')
+    ->withName('library_docs')
+    ->withController(Library::class)
+    ->withAction('docs')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/libraries/:library'))
-        ->withName('library')
-        ->withController(Library::class)
-        ->withAction('library')
-);
+$mapper->route($_root . '/libraries/:library')
+    ->withName('library')
+    ->withController(Library::class)
+    ->withAction('library')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/libraries/:library/:action'))
-        ->withController(Library::class)
-);
+$mapper->route($_root . '/libraries/:library/:action')
+    ->withName('library_action')
+    ->withController(Library::class)
+    ->defaults('action', 'library')
+    ->add();
 
 /* Development */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/development'))
-        ->withName('development')
-        ->withController(Development::class)
-        ->withAction('index')
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/development/:action'))
-        ->withController(Development::class)
-);
+$mapper->route($_root . '/development/:action')
+    ->withName('development')
+    ->withController(Development::class)
+    ->defaults('action', 'index')
+    ->add();
 
 /* Services */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/services'))
-        ->withName('services')
-        ->withController(Services::class)
-        ->withAction('index')
-);
+$mapper->route($_root . '/services')
+    ->withName('services')
+    ->withController(Services::class)
+    ->withAction('index')
+    ->add();
 
 /* Downloads */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/download'))
-        ->withName('download')
-        ->withController(Download::class)
-);
-
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/download/:app'))
-        ->withController(Download::class)
-        ->withAction('app')
-);
+$mapper->route($_root . '/download/:app')
+    ->withName('download')
+    ->withController(Download::class)
+    ->withAction('app')
+    ->add();
 
 /* Shop */
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/shop/us'))
-        ->withName('shopus')
-        ->withController(Shop::class)
-        ->withAction('us')
-);
+$mapper->route($_root . '/shop/us')
+    ->withName('shopus')
+    ->withController(Shop::class)
+    ->withAction('us')
+    ->add();
 
-$mapper->addRoute(
-    (new RouteBuilder($_root . '/shop/eu'))
-        ->withName('shopeu')
-        ->withController(Shop::class)
-        ->withAction('eu')
-);
+$mapper->route($_root . '/shop/eu')
+    ->withName('shopeu')
+    ->withController(Shop::class)
+    ->withAction('eu')
+    ->add();
