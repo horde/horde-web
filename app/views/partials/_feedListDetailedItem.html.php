716dc81a414cf83bd2def25abd2d0f4bb49bdf34
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-22 14:37:09 +0100

    fix: Simplify type check for feed items

--- app/views/partials/_feedListDetailedItem.html.php
+++ app/views/partials/_feedListDetailedItem.html.php
@@ -1,5 +1,5 @@
 <?php if (!isset($entry) && isset($locals['entry'])) { $entry = $locals['entry']; } ?>
-<?php if (is_object($entry) && method_exists($entry, 'link') && method_exists($entry, 'title') && method_exists($entry, 'description')): ?>
+<?php if ($entry instanceof Horde_Feed_Entry_Rss): ?>
 <li>
   <a href="<?php echo ($entry->link() == '' ? 'http://www.horde.org' : $entry->link()) ?>"><?php echo $entry->title() ?></a>
   <div class="description"><?php echo Horde_String::truncate(strip_tags($entry->description()), 180) ?></div>
