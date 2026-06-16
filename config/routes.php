830653a1384194cfec449071c45ee4a71d12b279
Ralf Lang <ralf.lang@ralf-lang.de>
2026-06-16 13:23:55 +0200

    refactor: Use buildRoute instead of route call

--- config/routes.php
+++ config/routes.php
@@ -18,102 +18,102 @@ use Horde\Hordeweb\Controller\Shop;
 $_root = ltrim(dirname($_SERVER['PHP_SELF']), '/');
 
 /* Home controller routes */
-$mapper->route($_root . '/')
+$mapper->buildRoute($_root . '/')
     ->withName('home')
     ->withController(Home::class)
     ->withAction('index')
     ->add();
 
-$mapper->route($_root . '/contact/')
+$mapper->buildRoute($_root . '/contact/')
     ->withName('contact')
     ->withController(Home::class)
     ->withAction('contact')
     ->add();
 
-$mapper->route($_root . '/thanks')
+$mapper->buildRoute($_root . '/thanks')
     ->withName('thanks')
     ->withController(Home::class)
     ->withAction('thanks')
     ->add();
 
-$mapper->route($_root . '/logos')
+$mapper->buildRoute($_root . '/logos')
     ->withName('logos')
     ->withController(Home::class)
     ->withAction('logos')
     ->add();
 
-$mapper->route($_root . '/410')
+$mapper->buildRoute($_root . '/410')
     ->withName('410')
     ->withController(Home::class)
     ->withAction('410')
     ->add();
 
-$mapper->route($_root . '/mail')
+$mapper->buildRoute($_root . '/mail')
     ->withName('mail')
     ->withController(Home::class)
     ->withAction('mail')
     ->add();
 
 /* Community */
-$mapper->route($_root . '/community/:action')
+$mapper->buildRoute($_root . '/community/:action')
     ->withName('community')
     ->withController(Community::class)
     ->defaults('action', 'index')
     ->add();
 
-$mapper->route($_root . '/community/team')
+$mapper->buildRoute($_root . '/community/team')
     ->withName('team')
     ->withController(Community::class)
     ->withAction('team')
     ->add();
 
-$mapper->route($_root . '/community/localization')
+$mapper->buildRoute($_root . '/community/localization')
     ->withName('localization')
     ->withController(Community::class)
     ->withAction('localization')
     ->add();
 
 /* Licenses */
-$mapper->route($_root . '/licenses/:action')
+$mapper->buildRoute($_root . '/licenses/:action')
     ->withName('licenses')
     ->withController(Licenses::class)
     ->defaults('action', 'index')
     ->add();
 
 /* Support */
-$mapper->route($_root . '/support')
+$mapper->buildRoute($_root . '/support')
     ->withName('support')
     ->withController(Support::class)
     ->withAction('index')
     ->add();
 
 /* Apps - main index aliases to h6 */
-$mapper->route($_root . '/apps')
+$mapper->buildRoute($_root . '/apps')
     ->withName('apps')
     ->withController(App::class)
     ->withAction('index')
     ->add();
 
 /* Apps - Horde 5 archive */
-$mapper->route($_root . '/apps/h5')
+$mapper->buildRoute($_root . '/apps/h5')
     ->withName('apps_h5')
     ->withController(App::class)
     ->withAction('h5')
     ->add();
 
-$mapper->route($_root . '/apps/h5/:app/docs/:file')
+$mapper->buildRoute($_root . '/apps/h5/:app/docs/:file')
     ->withName('app_h5_docs')
     ->withController(App::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/apps/h5/:app')
+$mapper->buildRoute($_root . '/apps/h5/:app')
     ->withName('app_h5')
     ->withController(App::class)
     ->withAction('app')
     ->add();
 
-$mapper->route($_root . '/apps/h5/:app/:action')
+$mapper->buildRoute($_root . '/apps/h5/:app/:action')
     ->withName('app_h5_action')
     ->withController(App::class)
     ->defaults('action', 'app')
@@ -122,25 +122,25 @@ $mapper->route($_root . '/apps/h5/:app/:action')
     ->add();
 
 /* Apps - Horde 6 */
-$mapper->route($_root . '/apps/h6')
+$mapper->buildRoute($_root . '/apps/h6')
     ->withName('apps_h6')
     ->withController(App::class)
     ->withAction('h6')
     ->add();
 
-$mapper->route($_root . '/apps/h6/:app/docs/:file')
+$mapper->buildRoute($_root . '/apps/h6/:app/docs/:file')
     ->withName('app_h6_docs')
     ->withController(App::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/apps/h6/:app')
+$mapper->buildRoute($_root . '/apps/h6/:app')
     ->withName('app_h6')
     ->withController(App::class)
     ->withAction('app')
     ->add();
 
-$mapper->route($_root . '/apps/h6/:app/:action')
+$mapper->buildRoute($_root . '/apps/h6/:app/:action')
     ->withName('app_h6_action')
     ->withController(App::class)
     ->defaults('action', 'app')
@@ -149,19 +149,19 @@ $mapper->route($_root . '/apps/h6/:app/:action')
     ->add();
 
 /* Apps - individual app routes (legacy, matches after h5/h6) */
-$mapper->route($_root . '/apps/:app/docs/:file')
+$mapper->buildRoute($_root . '/apps/:app/docs/:file')
     ->withName('app_docs')
     ->withController(App::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/apps/:app')
+$mapper->buildRoute($_root . '/apps/:app')
     ->withName('app')
     ->withController(App::class)
     ->withAction('app')
     ->add();
 
-$mapper->route($_root . '/apps/:app/:action')
+$mapper->buildRoute($_root . '/apps/:app/:action')
     ->withName('app_action')
     ->withController(App::class)
     ->defaults('action', 'app')
@@ -170,124 +170,124 @@ $mapper->route($_root . '/apps/:app/:action')
     ->add();
 
 /* Libraries - main index aliases to h6 */
-$mapper->route($_root . '/libraries')
+$mapper->buildRoute($_root . '/libraries')
     ->withName('libraries')
     ->withController(Library::class)
     ->withAction('index')
     ->add();
 
 /* Libraries - Horde 5 archive */
-$mapper->route($_root . '/libraries/h5')
+$mapper->buildRoute($_root . '/libraries/h5')
     ->withName('libraries_h5')
     ->withController(Library::class)
     ->withAction('h5')
     ->add();
 
-$mapper->route($_root . '/libraries/h5/:library/docs/:file')
+$mapper->buildRoute($_root . '/libraries/h5/:library/docs/:file')
     ->withName('library_h5_docs')
     ->withController(Library::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/libraries/h5/:library')
+$mapper->buildRoute($_root . '/libraries/h5/:library')
     ->withName('library_h5')
     ->withController(Library::class)
     ->withAction('library')
     ->add();
 
-$mapper->route($_root . '/libraries/h5/:library/:action')
+$mapper->buildRoute($_root . '/libraries/h5/:library/:action')
     ->withName('library_h5_action')
     ->withController(Library::class)
     ->defaults('action', 'library')
     ->add();
 
 /* Libraries - Horde 6 */
-$mapper->route($_root . '/libraries/h6')
+$mapper->buildRoute($_root . '/libraries/h6')
     ->withName('libraries_h6')
     ->withController(Library::class)
     ->withAction('h6')
     ->add();
 
-$mapper->route($_root . '/libraries/h6/:library/docs/:file')
+$mapper->buildRoute($_root . '/libraries/h6/:library/docs/:file')
     ->withName('library_h6_docs')
     ->withController(Library::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/libraries/h6/:library')
+$mapper->buildRoute($_root . '/libraries/h6/:library')
     ->withName('library_h6')
     ->withController(Library::class)
     ->withAction('library')
     ->add();
 
-$mapper->route($_root . '/libraries/h6/:library/:action')
+$mapper->buildRoute($_root . '/libraries/h6/:library/:action')
     ->withName('library_h6_action')
     ->withController(Library::class)
     ->defaults('action', 'library')
     ->add();
 
 /* Libraries - legacy routes (match after h5/h6) */
-$mapper->route($_root . '/libraries/:library/docs/:file')
+$mapper->buildRoute($_root . '/libraries/:library/docs/:file')
     ->withName('library_docs')
     ->withController(Library::class)
     ->withAction('docs')
     ->add();
 
-$mapper->route($_root . '/libraries/:library')
+$mapper->buildRoute($_root . '/libraries/:library')
     ->withName('library')
     ->withController(Library::class)
     ->withAction('library')
     ->add();
 
-$mapper->route($_root . '/libraries/:library/:action')
+$mapper->buildRoute($_root . '/libraries/:library/:action')
     ->withName('library_action')
     ->withController(Library::class)
     ->defaults('action', 'library')
     ->add();
 
 /* Development */
-$mapper->route($_root . '/development/:action')
+$mapper->buildRoute($_root . '/development/:action')
     ->withName('development')
     ->withController(Development::class)
     ->defaults('action', 'index')
     ->add();
 
 /* Services */
-$mapper->route($_root . '/services')
+$mapper->buildRoute($_root . '/services')
     ->withName('services')
     ->withController(Services::class)
     ->withAction('index')
     ->add();
 
 /* Downloads - Horde 5 archive */
-$mapper->route($_root . '/download/h5/:app')
+$mapper->buildRoute($_root . '/download/h5/:app')
     ->withName('download_h5')
     ->withController(Download::class)
     ->withAction('h5')
     ->add();
 
 /* Downloads - Horde 6 */
-$mapper->route($_root . '/download/h6/:app')
+$mapper->buildRoute($_root . '/download/h6/:app')
     ->withName('download_h6')
     ->withController(Download::class)
     ->withAction('h6')
     ->add();
 
 /* Downloads - legacy route aliases to H6 */
-$mapper->route($_root . '/download/:app')
+$mapper->buildRoute($_root . '/download/:app')
     ->withName('download')
     ->withController(Download::class)
     ->withAction('app')
     ->add();
 
 /* Shop */
-$mapper->route($_root . '/shop/us')
+$mapper->buildRoute($_root . '/shop/us')
     ->withName('shopus')
     ->withController(Shop::class)
     ->withAction('us')
     ->add();
 
-$mapper->route($_root . '/shop/eu')
+$mapper->buildRoute($_root . '/shop/eu')
     ->withName('shopeu')
     ->withController(Shop::class)
     ->withAction('eu')
